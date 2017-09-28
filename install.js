7caf41e469d0d97ec75838085a429953169ec64c
Levi Barros Nobrega <lbn@ug2c32.windows.cin.ufpe.br>
2017-09-28 14:56:36 -0300

    finish

--- install.js
+++ install.js
@@ -1,12 +1,14 @@
 var shell = require('shelljs');
 var fs = require('fs');
 var packages = JSON.parse(fs.readFileSync('extensions.json', 'utf8'));
-shell.exec('cp settings.json $HOME/.config/Code/User/settings.json;');
-var sh = [];
-packages.forEach((p)=>{
-    sh.push('code --install-extension '+ p);
+shell.exec('tar -xzvf Code.tar.gz;mv Code $HOME/.config/Code;code;cp settings.json $HOME/.config/Code/User/settings.json;', (s, o)=>{
+    var sh = [];
+    packages.forEach((p)=>{
+        sh.push('code --install-extension '+ p);
+    });
+    sh = sh.join(';');
+    shell.exec(sh, (status, output) => {
+        shell.exec('code;');
+    });
 });
-sh = sh.join(';');
-shell.exec(sh, (status, output) => {
-    
-});
+
