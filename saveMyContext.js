2a32277acb1aee951751afafa7bb0a2eaac0d7a5
Levi Barros Nobrega <lbn@ug2c32.windows.cin.ufpe.br>
2017-09-28 14:25:40 -0300

    finished

--- saveMyContext.js
+++ saveMyContext.js
@@ -1,6 +1,6 @@
 var shell = require('shelljs');
 var fs = require('fs');
-
+var filename = (process.argv[2])?process.argv[2]:'mylinuxvs';
 shell.exec('code --list-extensions;', (status, output) => {
     var e = output.split('\n');
     var ext = [];
@@ -9,6 +9,5 @@ shell.exec('code --list-extensions;', (status, output) => {
             ext.push(x);
     });
     fs.writeFileSync('extensions.json', JSON.stringify(ext));
-    
+    shell.exec('cp $HOME/.config/Code/User/settings.json settings.json;cd .. ;tar -czvf '+filename+'.tar.gz linuxvs;')
 });
-shell.exec('cp $HOME/.config/Code/User/settings.json settings.json;')
