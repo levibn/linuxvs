510f17b165e8a7384a873ea397bca2ef8fcdf709
Levi Barros Nobrega <lbn@ug2c32.windows.cin.ufpe.br>
2017-09-28 13:30:22 -0300

    new possibilities

---
+++ make.sh
@@ -0,0 +1,5 @@
+cp settings.json $HOME/.config/Code/User/settings.json
+code --install-extension 13xforever.language-x86-64-assembly
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.sublime-keybindings
+code
