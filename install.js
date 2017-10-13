9c8dd1630a86ce919edef02b419bfed4a1aa3fdd
Levi Barros Nobrega <lbn@ug4c23.windows.cin.ufpe.br>
2017-10-13 08:25:39 -0300

    new changes into make.sh

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 var shell = require('shelljs');
 var fs = require('fs');
 var packages = JSON.parse(fs.readFileSync('extensions.json', 'utf8'));
-shell.exec('tar -xzvf Code.tar.gz;mv Code $HOME/.config/Code;code;cp settings.json $HOME/.config/Code/User/settings.json;', (s, o)=>{
+shell.exec('cp settings.json $HOME/.config/Code/User/settings.json;', (s, o)=>{
     var sh = [];
     packages.forEach((p)=>{
         sh.push('code --install-extension '+ p);
